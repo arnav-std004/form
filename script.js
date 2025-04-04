ef2b5232185bc6e6f607952172dbf622dd03de60
Arnav Kharbanda <arnav.std004@gmail.com>
2025-04-04 19:13:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+(function() {
+    emailjs.init("jqEhZxDGvsNpfdbp4"); // Replace with your Email.js User ID
+})();
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let params = {
+        from_name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    emailjs.send("service_54esn64", "template_sqf5oh7", params)
+        .then(response => {
+            alert("We have received your message and will get back to you shortly!");  
+            document.getElementById("contactForm").reset();
+        })
+        .catch(error => {
+            alert("We are unable to recieve your request " + error);
+        });
+});
